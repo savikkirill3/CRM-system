bf04224d5c2068563b38d06651141336030b29db
Kirill Savik <savikkirill3@gmail.com>
2018-10-21 22:57:35 +0300

    Добавление и редактирование позиций

--- client/src/app/shared/interfaces.ts
+++ client/src/app/shared/interfaces.ts
@@ -13,3 +13,11 @@ export interface Category {
   user?: string
   _id: string
 }
+
+export interface Position {
+  name: string
+  cost: number
+  category: string
+  user?: string
+  _id?: string
+}
