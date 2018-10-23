792b1027c305e5079cabd3f72e7d449b61743fe1
Kirill Savik <savikkirill3@gmail.com>
2018-10-23 19:27:31 +0300

    Создание разметки страницы история

--- client/src/app/history-page/history-page.component.ts
+++ client/src/app/history-page/history-page.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {MaterialInstance, MaterialService} from "../shared/classes/material.service";
 
 @Component({
   selector: 'app-history-page',
   templateUrl: './history-page.component.html',
   styleUrls: ['./history-page.component.css']
 })
-export class HistoryPageComponent implements OnInit {
+export class HistoryPageComponent implements OnInit, OnDestroy,AfterViewInit {
+
+  @ViewChild('tooltip') tooltipRef: ElementRef
+  tooltip: MaterialInstance
+  isFilterVisible = false
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngOnDestroy() {
+    this.tooltip.destroy()
+  }
+
+  ngAfterViewInit() {
+    this.tooltip = MaterialService.initToolTip(this.tooltipRef)
+  }
 }
